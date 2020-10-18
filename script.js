45c7830d8e92323df124c24e824ae493d19262cf
Jackson Small <jackson40tall@gmail.com>
2020-10-18 14:00:03 -0600

    final touches

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // called when Submit is pressed on form
 document.getElementById("formSubmit").addEventListener("click", function (event) {
     event.preventDefault();
-    const value = document.getElementById("animalInput").value;
-    if (value === "")
+    // debugger
+    const nameValue = document.getElementById("nameInput").value;
+    const animalValue = document.getElementById("animalInput").value;
+    const colorValue = document.getElementById("colorInput").value;
+    if ((nameValue === "") || (animalValue === "") || (colorValue === ""))
         return;
-    console.log(value);
-    const id = (value * 31) % 672;
+    const id = ((nameValue.charCodeAt(0) + animalValue.charCodeAt(0) + colorValue.charCodeAt(0)) * 31) % 672;
     const url = "https://rickandmortyapi.com/api/character/" + id;
     fetch(url)
         .then(function (response) {
@@ -13,7 +15,7 @@ document.getElementById("formSubmit").addEventListener("click", function (event)
         }).then(function (json) {
             console.log(json);
             let results = "";
-            results += '<h2>YOU ARE: </h2><div id="results" class="flex-container"><div class="left-side">';
+            results += '<h2 id="you-are">YOU ARE : </h2><div id="results" class="flex-container"><div class="left-side">';
 
             results += '<img src="' + json.image + '"/></div><div class="right-side">';
 
